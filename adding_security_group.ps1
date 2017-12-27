21248f2f238cff156006acd5c1bdaa558b9622a2
486dx <hakanorcan@outlook.com>
2017-12-27 11:09:39 +0300

    fix adding_security_group.ps1

--- adding_security_group.ps1
+++ adding_security_group.ps1
@@ -1,6 +1,6 @@
 # adding all user of an OU to a security group
 Import-Module ActiveDirectory
-$users = Get-ADUser -Filter * -SearchBase "OU=sampleOU,DC=sample,DC=com"
+$users = Get-ADUser -Filter * -SearchBase "OU=sampleOU,DC=sampledomain,DC=com"
 foreach($user in $users)
 {
   Add-ADGroupMember -Identity targetSecurityGroupName -Member $user.samaccountname -ErrorAction SilentlyContinue
